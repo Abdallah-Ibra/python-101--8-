590640e279744b628cd8732440c6d9fc5ae2118a
Abdallah Ibrahim <just.4.work.2001@gmail.com>
2023-04-02 17:35:31 +0300

    VisionPlus8

--- test.py
+++ test.py
@@ -1,31 +1,82 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+from colorama import Fore
 
-
-def run(playwright: Playwright) -> None:
+def run(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
-
     page = context.new_page()
-
-    page.goto("https://www.amazon.com/")
-
-    page.get_by_placeholder("Search Amazon").click()
-
-    page.get_by_placeholder("Search Amazon").fill("macbook")
-
-    page.get_by_placeholder("Search Amazon").press("Enter")
-    page.wait_for_url("https://www.amazon.com/s?k=macbook&crid=N10P98QNN9AJ&sprefix=macbook%2Caps%2C475&ref=nb_sb_noss_1")
-
-    page.get_by_role("link", name="Apple 2022 MacBook Pro Laptop with M2 chip: 13-inch Retina Display, 8GB RAM, 256GB ​​​​​​​SSD ​​​​​​​Storage, Touch Bar, B...").click()
-    page.wait_for_url("https://www.amazon.com/2022-Apple-MacBook-Laptop-chip/dp/B0B3C5HNXJ/ref=sr_1_1?crid=N10P98QNN9AJ&keywords=macbook&qid=1679412519&sprefix=macbook%2Caps%2C475&sr=8-1")
-
-    page.get_by_role("listitem", name="Click to select 16 GB RAM").get_by_role("link").click()
-    page.wait_for_url("https://www.amazon.com/dp/B0B8TGK8W6/ref=twister_B0BB52CWDX")
+    page.set_default_timeout(10000)
+    username = "standard_user"
+    password= "secret_sauce"
+    page.goto("https://www.saucedemo.com/")
+    page.wait_for_timeout(300)
+    page.wait_for_load_state('domcontentloaded')
+    page.locator("[data-test=\"username\"]").click()
+    page.wait_for_load_state('domcontentloaded')
+    page.locator("[data-test=\"username\"]").fill(username)
+    page.wait_for_load_state('domcontentloaded')
+    page.locator("[data-test=\"password\"]").click()
+    page.wait_for_load_state('domcontentloaded')
+    page.locator("[data-test=\"password\"]").fill(password)
+    page.wait_for_load_state('domcontentloaded')
+    page.locator("[data-test=\"login-button\"]").click()
+    page.wait_for_timeout(500)
+    page.wait_for_load_state('domcontentloaded')
+    # if 'data-test="error"' in page.inner_html('*'):
+    #     print(Fore.RED+f'[-] Invalid Credits: {username}|{password}!'+Fore.RESET)
+    # else:
+    # #     print(Fore.LIGHTGREEN_EX+f'[+] Login Successfully: {username}|{password}!'+Fore.RESET)
+    # if page.url == 'https://www.saucedemo.com/inventory.html':
+    #     print(Fore.LIGHTGREEN_EX+f'[+] Login Successfully: {username}|{password}!'+Fore.RESET)
+    #     page.wait_for_timeout(300)
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]").click()
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("a:has-text(\"Sauce Labs Backpack\")").click()
+    #     page.wait_for_url("https://www.saucedemo.com/inventory-item.html?id=4")
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("a:has-text(\"1\")").click()
+    #     page.wait_for_url("https://www.saucedemo.com/cart.html")
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"checkout\"]").click()
+    #     page.wait_for_url("https://www.saucedemo.com/checkout-step-one.html")
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"firstName\"]").click()
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"firstName\"]").fill("Alex")
+    #     page.locator("[data-test=\"lastName\"]").click()
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"lastName\"]").fill("Matt")
+    #     page.locator("[data-test=\"postalCode\"]").click()
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"postalCode\"]").fill("00972")
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"continue\"]").click()
+    #     page.wait_for_url("https://www.saucedemo.com/checkout-step-two.html")
+    #     page.wait_for_load_state('domcontentloaded')
+    #     page.locator("[data-test=\"finish\"]").click()
+    #     if '/checkout-complete.html' in page.url:
+    #         print(Fore.CYAN+'[#] Your Order Completed!'+Fore.RESET)
+    # else:
+    #     print(Fore.RED+f'[-] Invalid Credits: {username}|{password}!'+Fore.RESET)
+    img = page.locator('img[alt="Sauce Labs Backpack"]').screenshot(type='png')
+    with open('SauceLabsBackPack/img1.png','wb') as f:
+        f.write(img)
+    body = page.locator('#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.inventory_item_label > div').text_content()
+    price = page.locator('#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > div').text_content()
+    print('[#] Title: Sauce Labs Backpack')
+    print('='*30)
+    print(f'[#] Body:\n{body}')
+    print('='*30)
+    print(f'[#] Price: {price}')
+    
+    # page.get_by_role("heading", name="Thank you for your order!").click()
     page.pause()
     # ---------------------
     context.close()
     browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+with sync_playwright() as p:
+    run(p)
+
